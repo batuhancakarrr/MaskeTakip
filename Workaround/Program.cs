e8dfee3d20d1c6ecbaafec6522132c0d347d2f26
Batuhan Çakar <batuyhan19@gmail.com>
2024-04-08 01:24:17 +0300

    MaskeTakip

---
+++ Workaround/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Business.Concrete;
+using Entities;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "BATUHAN";
+            person.LastName = "ÇAKAR";
+            person.DateOfBirthYear = 2001;
+            person.NationalIdentity = 1;
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person);
+
+            Console.ReadLine();
+        }
+    }
+}
